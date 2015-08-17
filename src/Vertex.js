047d4e95ee2ba6037bd3f4df5bf629dbdab4dfa2
Tim PILLARD <tim@tpillard.fr>
2015-08-17 22:51:18 +0200

    Fix typo

--- src/Vertex.js
+++ src/Vertex.js
@@ -2,7 +2,7 @@ import Vector from './Vector';
 
 export default class Vertex {
   constructor(vector) {
-    vector instanceof vector || 
+    vector instanceof Vector ||
     () => { throw new PompeiError(`Bad Parameters: constructor(vector)`); }
 
     this._vector = vector;
